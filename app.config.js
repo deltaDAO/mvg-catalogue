f5f4becc9ffa70582c8d4329f9944a580bd3aa41
Moritz Kirstein <mkirstein@protonmail.com>
2022-01-06 14:09:05 +0000

    refactor: adjust app.config

--- app.config.js
+++ app.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   metadataCacheUri:
     process.env.METADATA_CACHE_URI || 'https://aquarius.delta-dao.com',
-  chainId: 2021000
+  chainId: process.env.CHAIN_ID || 2021000,
+  portalUri: process.env.PORTAL_URI || 'https://portal.minimal-gaia-x.eu'
 }
