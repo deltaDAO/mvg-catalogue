639cb2b29aae41546e5c2ce97b18533798038c1e
Luca Milanese <luca.milanese90@gmail.com>
2022-01-24 16:00:35 +0100

    feature: add home link to logo

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -1,7 +1,14 @@
 import React, { ReactElement } from 'react'
 import styles from './Logo.module.css'
 import GaiaXLogo from '../../../public/images/gaia-x-logo.svg'
+import Link from 'next/link'
 
 export default function Logo(): ReactElement {
-  return <GaiaXLogo className={styles.logo} />
+  return (
+    <Link href="/">
+      <a>
+        <GaiaXLogo className={styles.logo} />
+      </a>
+    </Link>
+  )
 }
