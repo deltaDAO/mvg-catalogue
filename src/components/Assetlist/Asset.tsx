671d1d565e5bcf2cc4dbaf2d90e3b9f0e2777d52
Luca Milanese <luca.milanese90@gmail.com>
2022-01-29 11:21:03 +0100

    refactor: use portalUri env for asset external link

--- src/components/Assetlist/Asset.tsx
+++ src/components/Assetlist/Asset.tsx
@@ -4,6 +4,7 @@ import { format } from 'date-fns'
 import Dotdotdot from 'react-dotdotdot'
 import styles from './Asset.module.css'
 import Link from 'next/link'
+import { portalUri } from '../../../app.config'
 
 export default function Asset({
   asset
@@ -11,7 +12,7 @@ export default function Asset({
   asset: MetadataMain
 }): ReactElement {
   return (
-    <Link href={`https://portal.minimal-gaia-x.eu/asset/${asset._id}`}>
+    <Link href={`${portalUri}/asset/${asset._id}`}>
       <a className={styles.asset} target="blank" rel="noopener noreferrer">
         <Dotdotdot className={styles.name} clamp={1}>
           {asset.name}
