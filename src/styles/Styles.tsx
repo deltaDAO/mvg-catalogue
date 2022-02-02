60c1754cf92b2c9e3854987700c9eb0a488724c0
Luca Milanese <luca.milanese90@gmail.com>
2022-02-02 15:52:07 +0100

    feat: install overpass font

--- src/styles/Styles.tsx
+++ src/styles/Styles.tsx
@@ -1,6 +1,10 @@
 import { ReactElement, ReactNode } from 'react'
 import { ToastContainer } from 'react-toastify'
 
+import '@fontsource/overpass/300.css'
+import '@fontsource/overpass/400.css'
+import '@fontsource/overpass/700.css'
+
 export default function Styles({
   children
 }: {
