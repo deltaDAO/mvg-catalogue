3945fd383e73deb564ff838b2eff80a9d50d3333
Luca Milanese <luca.milanese90@gmail.com>
2022-03-21 20:06:21 +0100

    feat: update pregenerate script and gitignore file

--- scripts/pregenerate.sh
+++ scripts/pregenerate.sh
@@ -3,6 +3,9 @@
 # Write out repo metadata
 node ./scripts/write-repo-metadata > content/repo-metadata.json
 
+# Run post install script
+npm run postinstall
+
 # Generate Apollo typings
 npm run apollo:codegen
 
